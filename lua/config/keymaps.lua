89de2dad122a7c11c6d98313656d6f908153c67c
Fernando Herrera <cafeherval@gmail.com>
2025-09-17 14:33:06 -0600

    Latex stuff

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("i", "jk", "<Esc>", { desc = "jk -> esc" })
+vim.keymap.set("i", "kj", "<Esc>", { desc = "kj -> esc" })
